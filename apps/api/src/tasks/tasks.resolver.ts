e825a353e5e24e87643270e09470992b72e8d14a
holasoynaoki <naoki13futsal@gmail.com>
2022-09-12 16:18:09 +0900

    :sparkles:tweak

--- apps/api/src/tasks/tasks.resolver.ts
+++ apps/api/src/tasks/tasks.resolver.ts
@@ -1,4 +1,33 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Task } from './../entities/task.entity';
+import { TasksService } from './tasks.service';
 
 @Resolver()
-export class TasksResolver {}
+export class TasksResolver {
+  constructor(private tasksService: TasksService) {}
+
+  @Query(() => [Task])
+  tasks() {
+    return this.tasksService.findAll();
+  }
+
+  @Query(() => Task)
+  task(@Args('id') id: number) {
+    return this.tasksService.findOne(id);
+  }
+
+  @Mutation(() => Task)
+  createTask(@Args('name') name: string) {
+    return this.tasksService.createTask(name);
+  }
+
+  @Mutation(() => Task)
+  deleteTask(@Args('id') id: number) {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Mutation(() => Task)
+  updateTask(@Args('id') id: number, @Args('name') name: string) {
+    return this.tasksService.updateTask(id, name);
+  }
+}
