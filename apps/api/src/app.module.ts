27880ac741d72d983f4f518868624fb3f249d95f
holasoynaoki <naoki13futsal@gmail.com>
2022-09-12 11:17:59 +0900

    :sparkles:tweak

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true, // prodではoffにする
+      debug: true, // prodではoffにする
+      autoSchemaFile: join(process.cwd(), '../graphql/schema.gql'), // MEMO: productionではschema自動生成はしない
+      typePaths: undefined, // MEMO: productionでは生成済みのschemaを使う
+    }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
